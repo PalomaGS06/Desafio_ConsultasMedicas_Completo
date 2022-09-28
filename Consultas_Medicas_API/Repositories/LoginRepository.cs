da2c672dce343f06aeb9240e6b8bc61da22f480b
Paloma Souza <paloma.souza112@gmail.com>
2022-09-28 01:37:04 -0300

    Claims adicionadas no repositório do Login

--- Consultas_Medicas_API/Repositories/LoginRepository.cs
+++ Consultas_Medicas_API/Repositories/LoginRepository.cs
@@ -2,7 +2,11 @@
 using APIConsultasMedicas.Models;
 using ConsultaMedicaVet.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 
 namespace APIConsultasMedicas.Repositories
 {
@@ -25,12 +29,42 @@ namespace APIConsultasMedicas.Repositories
             if (usuario != null && login.Senha != null && usuario.Senha.Contains("$2b$")) // Condição para a validação da senha
             {
                 bool validada = BCrypt.Net.BCrypt.Verify(login.Senha, usuario.Senha); // Faz a verificação do que foi salvo. Método aplicado ao logar
-               
+                                                                                      // Criar as credenciais do JWT
+
+                // Realizando a definição da Claim
+                var myClaims = new[]
+                {
+                        new Claim(JwtRegisteredClaimNames.Email, usuario.Email),
+                        new Claim(ClaimTypes.Role, "Paciente"),
+
+                        new Claim("Cargo", usuario.TipoUsuario.Tipo) // indica qual o cargo (tipo) de usuário
+
+                    };
+
+                // Chaves criadas através da Issuer que foi feita na configuração do JWT no Startup
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("Api-consultas-key"));
+
+                // Credenciais criadas
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                // Token gerado atraves das informações passadas na configuração do JWT
+                var myToken = new JwtSecurityToken(
+                    issuer: "consultasMedicas.webAPI",  // local da emissão do token
+                    audience: "consultasMedicas.webAPI",
+                    claims: myClaims,
+                    expires: DateTime.Now.AddMinutes(30), // tempo de expiração do token
+                    signingCredentials: creds
+                    );
+
+                return new JwtSecurityTokenHandler().WriteToken(myToken);
+
             }
-
+            
             return null;
-
         }
 
-    }     
+           
+    }
+
+ 
 }
