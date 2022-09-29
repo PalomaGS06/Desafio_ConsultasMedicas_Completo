24e61e08e1fa8f9f64c0c5a8a4efa3e7c56b78ef
Paloma Souza <paloma.souza112@gmail.com>
2022-09-29 02:09:20 -0300

    Pastas do Projeto de Testes atualizadas

--- Consultas_Medicas_Tests/Controllers/AdministradorControllerTests.cs
+++ Consultas_Medicas_Tests/Controllers/AdministradorControllerTests.cs
@@ -3,11 +3,6 @@ using APIConsultasMedicas.Interfaces;
 using ConsultaMedicaVet.Models;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Consultas_Medicas_Tests.Controllers
