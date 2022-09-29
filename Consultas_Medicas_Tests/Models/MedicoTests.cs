aba6fac3aa87a007c84c09c322b72d3f5d0287cb
Paloma Souza <paloma.souza112@gmail.com>
2022-09-29 02:57:09 -0300

    Models de Testes criadas para cada classe

---
+++ Consultas_Medicas_Tests/Models/MedicoTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultas_Medicas_Tests.Models
+{
+    public class MedicoTests
+    {
+    }
+}
